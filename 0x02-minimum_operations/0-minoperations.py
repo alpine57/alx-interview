de1eef1eb6b3bbdc46b3a352e518e2ef356dd06f
alpine57 <kamutileeon@.com>
2024-08-07 13:55:20 -0700

    minimum operations done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+def minOperations(n):
+"""Gets fewest # of operations needed to result in exactly n H characters
+"""
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
