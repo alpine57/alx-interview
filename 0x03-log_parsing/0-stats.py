70fc2cce042c2c4a0ae3da2ed70cfe052a1cc842
alpine57 <kamutileeon@.com>
2024-08-16 13:33:06 -0700

    logged

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,10 +7,12 @@ def print_msg(dict_sc, total_file_size):
     """
     Method to print
     Args:
-        dict_sc: dict of status code
+        dict_sc: dict of status codes
+        total_file_size: total of the file
     Returns:
         Nothing
     """
+
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
@@ -18,6 +20,7 @@ def print_msg(dict_sc, total_file_size):
 
 
 total_file_size = 0
+code = 0
 counter = 0
 dict_sc = {"200": 0,
            "301": 0,
@@ -30,29 +33,22 @@ dict_sc = {"200": 0,
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        
-        if len(parsed_line) < 7:
-            continue
-        
-        try:
-        
-            file_size = int(parsed_line[-1])
-            status_code = parsed_line[-2]
-            
-            total_file_size += file_size
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
             counter += 1
 
-            if status_code in dict_sc:
-                dict_sc[status_code] += 1
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-            if counter == 10:
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
                 print_msg(dict_sc, total_file_size)
                 counter = 0
 
-        except (ValueError, IndexError):
-            continue
-
 finally:
     print_msg(dict_sc, total_file_size)
-
